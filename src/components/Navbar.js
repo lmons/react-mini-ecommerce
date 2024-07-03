55e74f53e7b7039d4cb1ebfcfa8b9d7ec2733821
unknown <monsif.hamoumi@gmail.com>
2024-07-03 14:24:30 +0100

    add icon to navbar to show number of products in the cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { CartContext } from "../context/CartContext";
 
 function Navbar() {
+  const { cartItems } = useContext(CartContext);
+
+  const cartItemCount = cartItems ? cartItems.length : 0;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">React E-commerce</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/">
+          React E-commerce
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Products</Link>
+              <Link className="nav-link" to="/">
+                Products
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/cart">Cart</Link>
+              <Link className="nav-link position-relative" to="/cart">
+                Cart{" "}
+                {cartItemCount > 0 && (
+                  <span className="badge bg-primary rounded-pill">
+                    {cartItemCount}
+                    <FaShoppingCart className="ms-1" />
+                  </span>
+                )}
+                
+              </Link>
             </li>
           </ul>
         </div>
