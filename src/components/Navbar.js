8c644c98973c9541282890942fd0e326d3f24ed3
unknown <monsif.hamoumi@gmail.com>
2024-07-03 13:51:17 +0100

    change NavBar style to show hamburger menu in small screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Mini E-commerce with react</Link>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Products</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/cart">Cart</Link>
-          </li>
-        </ul>
+      <div className="container">
+        <Link className="navbar-brand" to="/">React E-commerce</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
